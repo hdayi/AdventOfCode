f88f40e531a7d77bf9fb52d96da4f6c417c24dce
HACI DAYI <hdayi@hotmail.com>
2023-12-05 22:59:43 +0300

    2023 day03 step 1 is not ok

--- src/main/java/tr/com/d3kod/y_2023/day03/Day03.java
+++ src/main/java/tr/com/d3kod/y_2023/day03/Day03.java
@@ -1,6 +1,11 @@
 package tr.com.d3kod.y_2023.day03;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import tr.com.d3kod.util.ReadFile;
 
@@ -14,27 +19,59 @@ public class Day03 {
   public static void run() {
     System.out.println("Hello from 2023 day 03");
     input = ReadFile.readFile("23_day03.txt");
+    // input = new ArrayList<>(
+    // Arrays.asList(
+    // "467..114..",
+    // "...*......",
+    // "..35..633.",
+    // "......#...",
+    // "617*......",
+    // ".....+.58.",
+    // "..592.....",
+    // "......755.",
+    // "...$.*....",
+    // ".664.598.."));
     stepOne();
   }
 
   private static void stepOne() {
+    List<String> liste = new ArrayList<>();
     int sum = 0;
+    Pattern p = Pattern.compile("\\d+");
 
     for (int i = 0; i < input.size(); i++) {
       String line = input.get(i);
-      for (int j = 0; j < line.length(); j++) {
-        if (Character.isDigit(line.charAt(j))) {
 
+      Matcher m = p.matcher(line);
+
+      while (m.find()) {
+        String match = m.group(0);
+        int j = line.indexOf(match);
+        if (isValidPart(i, j, match)) {
+          sum += Integer.parseInt(match);
         }
       }
-      System.out.println("");
-
     }
 
     System.out.println("2023 Day 03 step 1: " + sum);
   }
 
-  private static void int foundDigit(int i, int j){
-    
+  private static boolean isValidPart(int i, int j, String num) {
+    int satirStart = i == 0 ? 0 : i - 1;
+    int sutunStart = j == 0 ? 0 : j - 1;
+    // bu dahil;
+    int satirSon = i == input.size() - 1 ? i : i + 1;
+    int sutunSon = j + num.length() >= input.get(i).length() - 1 ? input.get(i).length() - 1 : j + num.length();
+
+    for (int k = satirStart; k <= satirSon; k++) {
+      for (int l = sutunStart; l <= sutunSon; l++) {
+        char c = input.get(k).charAt(l);
+        if (!Character.isDigit(c) && c != '.') {
+          return true;
+        }
+      }
+    }
+    return false;
   }
+
 }
