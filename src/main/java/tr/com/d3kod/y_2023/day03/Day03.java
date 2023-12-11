a873972176ead861c6c4e9571aab200a37493431
HACI DAYI <hdayi@hotmail.com>
2023-12-11 23:46:27 +0300

    2023 Day03 step 1 ok

--- src/main/java/tr/com/d3kod/y_2023/day03/Day03.java
+++ src/main/java/tr/com/d3kod/y_2023/day03/Day03.java
@@ -16,24 +16,12 @@ public class Day03 {
   public static void run() {
     System.out.println("Hello from 2023 day 03");
     input = ReadFile.readFile("23_day03.txt");
-    // input = new ArrayList<>(
-    // Arrays.asList(
-    // "467..114..",
-    // "...*......",
-    // "..35..633.",
-    // "......#...",
-    // "617*......",
-    // ".....+.58.",
-    // "..592.....",
-    // "......755.",
-    // "...$.*....",
-    // ".664.598.."));
     stepOne();
+
   }
 
   private static void stepOne() {
     int sum = 0;
-    int sum2 = 0;
     Pattern p = Pattern.compile("\\d+");
 
     for (int i = 0; i < input.size(); i++) {
@@ -43,16 +31,21 @@ public class Day03 {
 
       while (m.find()) {
         String match = m.group(0);
-        sum2 += Integer.parseInt(match);
         int j = line.indexOf(match);
         if (isValidPart(i, j, match)) {
           sum += Integer.parseInt(match);
         }
+        String replacement = "";
+        // If same number is repeating in the same line it finds first one so
+        // we must replace every number we find with same amount of dots
+        for (int k = 0; k < match.length(); k++) {
+          replacement += ".";
+        }
+        line = line.replaceFirst(match, replacement);
       }
     }
 
     System.out.println("2023 Day 03 step 1: \t\t" + sum);
-    System.out.println("2023 Day 03 step 1 all of it: \t" + sum2);
   }
 
   private static boolean isValidPart(int i, int j, String num) {
@@ -70,14 +63,19 @@ public class Day03 {
         ? input.get(i).length() - 1
         : j + num.length();
 
-    String line = input.get(satirStart).substring(sutunStart, sutunSon + 1);
-    if (satirStart + 1 <= satirSon) {
-      line += input.get(satirStart + 1).substring(sutunStart, sutunSon + 1);
+    String line = "";
+    try {
+      line = input.get(satirStart).substring(sutunStart, sutunSon + 1);
+      if (satirStart + 1 <= satirSon) {
+        line += input.get(satirStart + 1).substring(sutunStart, sutunSon + 1);
+      }
+      if (satirStart + 2 <= satirSon) {
+        line += input.get(satirStart + 2).substring(sutunStart, sutunSon + 1);
+      }
+      line = line.replaceAll("\\.", "").replaceAll("\\d", "");
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    if (satirStart + 2 <= satirSon) {
-      line += input.get(satirStart + 2).substring(sutunStart, sutunSon + 1);
-    }
-    line = line.replaceAll("\\.", "").replaceAll("\\d", "");
 
     return line.length() > 0;
   }
