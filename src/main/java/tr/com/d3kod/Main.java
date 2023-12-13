653db5bfa338595df282ab102bc7c1192704bd29
HACI DAYI <hdayi@hotmail.com>
2023-12-13 15:16:31 +0300

    2023 Day 4 starting

--- src/main/java/tr/com/d3kod/Main.java
+++ src/main/java/tr/com/d3kod/Main.java
@@ -3,7 +3,7 @@ package tr.com.d3kod;
 public class Main {
 
   public static void main(String[] args) {
-    tr.com.d3kod.y_2023.day03.Day03.run();
+    tr.com.d3kod.y_2023.day04.Day04.run();
 
   }
 
---
+++ src/main/java/tr/com/d3kod/y_2023/day04/Day04.java
@@ -0,0 +1,7 @@
+package tr.com.d3kod.y_2023.day04;
+
+public class Day04 {
+  public static void run() {
+    System.out.println("Hello from 2023 day 4");
+  }
+}
